fb039d38f2fab6a667e12b851f872242c45e9ed1
Saurav <imsauravroy007@outlook.com>
2020-06-21 01:11:22 +0530

    added files

---
+++ Scenerio-3/ToyTrex/Controllers/ToyTrexWebApi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ToyTrex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToyTrexWebApi : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ToyTrexWebApi> _logger;
+
+        public ToyTrexWebApi(ILogger<ToyTrexWebApi> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ToyTrexMain> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new ToyTrexMain
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
